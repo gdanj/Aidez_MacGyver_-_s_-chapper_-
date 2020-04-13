fcc1582997988d717d26096e04923a61d5577db8
gdanj <gdanj21@gmail.com>
2020-04-13 18:39:26 +0200

    ajout des fleche dans les event

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import pygame
+
 pygame.init()
 
 
 # ouvrire la fenetre
-window = pygame.display.set_mode((600, 600))
+window = pygame.display.set_mode((400, 400))
+fond = pygame.image.load("background.jpg")
+window.blit(fond, (0,0))
 
-continue = from django.utils.translation import ugettext_lazy as _
+pygame.display.flip()
+
+#BOUCLE INFINIE
+test = True
+
+
+#Boucle infinie
+while test:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            test = False
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_LEFT :
+                print("Espace")
+            if event.key == pygame.K_UP :
+                print("Espace")
+            if event.key == pygame.K_RIGHT :
+                print("Espace")
+            if event.key == pygame.K_DOWN :
+                print("Espace")
+        
