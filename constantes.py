6c2698f1fe57bd1a5b13cc14a7a0ecabbae09099
gdanj <gdanj21@gmail.com>
2020-04-13 21:27:22 +0200

    ajout du fichier des constants et de l'image tube

---
+++ constantes.py
@@ -0,0 +1,17 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "DK Labyrinthe"
+image_icone = "pictures/MacGyver.png"
+
+#Listes des images du jeu
+image_mur = "pictures/floor.png"
+image_arrivee = "pictures/Gardien.png"
+image_potion = "pictures/potion.png"
+image_aiguille = "pictures/aiguille.png"
+image_tube = "pictures/tube.png"
--- main.py
+++ main.py
@@ -13,6 +13,19 @@ pygame.display.flip()
 #BOUCLE INFINIE
 test = True
 
+with open("map.txt", "r") as fichier:
+	structure_niveau = []
+	#On parcourt les lignes du fichier
+	for ligne in fichier:
+		ligne_niveau = []
+		#On parcourt les sprites (lettres) contenus dans le fichier
+		for sprite in ligne:
+			#On ignore les "\n" de fin de ligne
+			if sprite != '\n':
+				#On ajoute le sprite à la liste de la ligne
+				ligne_niveau.append(sprite)
+		#On ajoute la ligne à la liste du niveau
+		structure_niveau.append(ligne_niveau)
 
 #Boucle infinie
 while test:
